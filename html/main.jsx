ba4fb9e1f486db607a5ad908be3a3fcb57083a6d
Lukas Cedronas <lukascedronas1@gmail.com>
2018-08-04 12:37:43 +0300

    Initial commit

---
+++ html/main.jsx
@@ -0,0 +1,132 @@
+class Signin extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleEmailChange = this.handleEmailChange.bind(this);
+		this.handlePasswordChange = this.handlePasswordChange.bind(this);
+		this.signIn = this.signIn.bind(this);
+		this.state = {
+		  email:'',
+		  password:''
+		};
+	}
+	
+   render() {
+	  return (
+		<div>
+		  <form className="form-signin">
+			<h2 className="form-signin-heading">Please sign in</h2>
+			<label for="inputEmail" className="sr-only">Email address</label>
+			<input type="email" onChange={this.handleEmailChange} id="inputEmail" className="form-control" placeholder="Email address" required autofocus />
+			<label for="inputPassword" className="sr-only">Password</label>
+			<input type="password" onChange={this.handlePasswordChange} id="inputPassword" className="form-control" placeholder="Password" required />
+			 
+			<button className="btn btn-lg btn-primary btn-block" onClick={this.signIn} type="button">Sign in</button>
+		  </form>
+		  <div>
+			<Link to="/signup">{'Signup'}</Link>
+		  </div>
+		</div>
+	 
+		)
+	}
+	 
+	signIn(){
+		axios.post('/signin', {
+		  email: this.state.email,
+		  password: this.state.password
+		})
+		.then(function (response) {
+		  console.log(response);
+		})
+		.catch(function (error) {
+		  console.log(error);
+		});            
+	}
+	 
+	handleEmailChange(e){
+		this.setState({email:e.target.value})
+	}
+	handlePasswordChange(e){
+		this.setState({password:e.target.value})
+	}
+	
+}
+
+class Signup extends React.Component{
+		constructor(props) {
+		super(props);
+		this.handleEmailChange = this.handleEmailChange.bind(this);
+		this.handlePasswordChange = this.handlePasswordChange.bind(this);
+		this.handleNameChange = this.handleNameChange.bind(this);
+
+		this.signUp = this.signUp.bind(this);
+		this.state = {
+		  email:'',
+		  password:'',
+		  name:''
+		};
+	}
+	
+  render() {
+      return (
+        <div>
+          <form className="form-signin">
+            <h2 className="form-signin-heading">Please sign up</h2>
+            <label for="inputName" className="sr-only">Name</label>
+            <input type="name" onChange={this.handleNameChange} id="inputName" className="form-control" placeholder="Name" required autofocus />
+            <label for="inputEmail" className="sr-only">Email address</label>
+            <input type="email" onChange={this.handleEmailChange} id="inputEmail" className="form-control" placeholder="Email address" required autofocus />
+            <label for="inputPassword" className="sr-only">Password</label>
+            <input type="password" onChange={this.handlePasswordChange} id="inputPassword" className="form-control" placeholder="Password" required />
+             
+            <button className="btn btn-lg btn-primary btn-block" onClick={this.signUp} type="button">Sign up</button>
+          </form>
+		   <div>
+			<Link to="/">{'Signin'}</Link>
+		  </div>
+        </div>
+         
+      )
+    }
+	
+	signUp(){
+		axios.post('/signup', {
+		  name: this.state.name,
+		  email: this.state.email,
+		  password: this.state.password
+		})
+		.then(function (response) {
+		  console.log(response);
+		})
+		.catch(function (error) {
+		  console.log(error);
+		});            
+	}
+	
+	 handleNameChange(e){
+		this.setState({name:e.target.value})
+	}
+	handleEmailChange(e){
+		this.setState({email:e.target.value})
+	}
+	handlePasswordChange(e){
+		this.setState({password:e.target.value})
+	}
+}
+
+var Router = window.ReactRouter.Router;
+var Route = window.ReactRouter.Route;
+var hashHistory = window.ReactRouter.hashHistory;
+var Link = window.ReactRouter.Link;
+
+ReactDOM.render( <
+Signin / > ,
+document.getElementById('app')
+);
+
+ReactDOM.render(
+    <Router history={hashHistory}>
+        <Route component={Signin} path="/"></Route>
+        <Route component={Signup} path="/signup"></Route>
+    </Router>,
+document.getElementById('app'));
